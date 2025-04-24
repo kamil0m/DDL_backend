7412e0e1c5f03a20476b7df18ff1775fff03f5b6
Kamil Markowski <kamil0markowski@gmail.com>
2025-04-24 16:32:08 +0200

    backend exported to a seperate git repository

---
+++ api/send-email.js
@@ -0,0 +1,46 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT_MAIL || 5000;
+app.use(cors());
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+app.post('/send-email', (req, res) => {
+    const { name, email, message } = req.body;
+
+    const mailOptions = {
+        from: `"${name}" <${process.env.EMAIL_USER}>`,
+        to: process.env.EMAIL_TO,
+        subject: `Wiadomość od ${name}`,
+        replyTo: email,
+        text: `Imię i nazwisko/Nazwa: ${name}\nEmail: ${email}\n\nWiadomość:\n${message}`,
+    };
+
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Błąd podczas wysyłania:', error);
+            res.status(500).send({ message: 'Błąd przy wysyłce wiadomości.' });
+        } else {
+            console.log('Wiadomość wysłana: ' + info.response);
+            res.status(200).send({ message: 'Wiadomość została wysłana pomyślnie!' });
+        }
+    });
+});
+app.listen(port, () => {
+    console.log(`Serwer działa na http://localhost:${port}`);
+});
---
+++ config/middlewares.ts
@@ -0,0 +1,30 @@
+import { dir } from "console";
+import { connect } from "http2";
+import src from "../src";
+
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  // 'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
