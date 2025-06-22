12bce7464e01d1bf583839540fc3ed00772880a0
Kamil Markowski <kamil0markowski@gmail.com>
2025-06-22 23:32:41 +0200

    custom component deleted

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,20 +1,3 @@
-import type { Schema, Struct } from '@strapi/strapi';
-
-export interface CtaCallToAction extends Struct.ComponentSchema {
-  collectionName: 'components_cta_call_to_actions';
-  info: {
-    displayName: 'CallToAction';
-  };
-  attributes: {
-    Link: Schema.Attribute.String & Schema.Attribute.Required;
-    Wezwanie: Schema.Attribute.Text & Schema.Attribute.Required;
-  };
-}
-
-declare module '@strapi/strapi' {
-  export module Public {
-    export interface ComponentSchemas {
-      'cta.call-to-action': CtaCallToAction;
-    }
-  }
-}
+/*
+ * The app doesn't have any components yet.
+ */
